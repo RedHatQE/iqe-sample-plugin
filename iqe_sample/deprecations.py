d1c145dd04901b2478c4fda81f194912b68db463
Nikhil Dhandre <ndhandre@redhat.com>
2026-09-08 15:34:16 +0530

    iqe-sample-plugin > iqe-wheels

--- iqe_sample/deprecations.py
+++
@@ -1,9 +0,0 @@
-from iqe.deprecations import deprecator
-
-deprecated = deprecator(plugin_package_name="iqe-sample-plugin")
-
-# DEPRECATE_SOMETHING_GOOD = deprecated(
-#    "Feature X has been deprecated",
-#    "There is no replacement",
-#    "0.1.0"
-# )
--- iqe_sample/tests/test_plugin.py
+++
@@ -1,2 +0,0 @@
-def test_plugin_accessible(app):
-    app.sample
